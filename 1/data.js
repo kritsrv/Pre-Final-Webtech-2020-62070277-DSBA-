8efad08f29cb90de122c8ff992c7aa64e7598583
กริชกิตกิจกฤษกฤตส่วนมากคนชอบเรียกกริชว่ากิตแต่กริชไม่ถือ <54878501+kritsrv@users.noreply.github.com>
2020-11-18 21:25:05 +0700

    Add files via upload

--- 1/data.js
+++ 1/data.js
@@ -8,22 +8,20 @@ request.onreadystatechange = function() {
 request.open("GET", requestURL, true);
 request.send();
 
-var result = '';
+var result = '<div class="row">';
 
 function dataReportStatus(data) {
-    console.log(data.tracks.items.length);
+    // console.log(data.tracks.items.length);
     for (let i = 0; i < data.tracks.items.length; i++) {
-        if (i % 3 == 0) {
-            result += '<div class="row">';
-            console.log(result);
+        if ((i >= 3) && (i % 3 == 0)) {
+            result += '</div><br><div class="row">';
+            // console.log(result);
         }
-        for (let j = 0; j < data.tracks.items.length; j++) {
-            result += '<div class="col-md-4"><div class="card" style="width: 23rem;"><img src="' + data.tracks.items[j].album.images[0].url + '" class="card-img-top" width="' + data.tracks.items[j].album.images[0].width + '"';
-            result += '<div class="card-body"><h5 class="card-title">&nbsp;&nbsp;' + data.tracks.items[j].album.name + '</h5><p class="card-subtitle">&nbsp;&nbsp;Artist: ' + data.tracks.items[j].album.artists[0].name + '</p> <br>';
-            result += '<p class="card-subtitle">&nbsp;&nbsp;Release date: ' + data.tracks.items[j].album.release_date + '</p> <br>';
-            result += '<p class="card-subtitle">&nbsp;&nbsp;Avaliable: ' + parseInt(data.tracks.items[j].album.available_markets.length) + ' countries</p> <br></div></div><br>';
-            console.log(result);
-        }
+        result += '<div class="col-md-4"><div class="card" style="width: 23rem;"><img src="' + data.tracks.items[i].album.images[0].url + '" class="card-img-top" width="' + data.tracks.items[i].album.images[0].width + '"';
+        result += '<div class="card-body"><h5 class="card-title">&nbsp;&nbsp;' + data.tracks.items[i].album.name + '</h5><p class="card-subtitle">&nbsp;&nbsp;Artist: ' + data.tracks.items[i].album.artists[0].name + '</p> <br>';
+        result += '<p class="card-subtitle">&nbsp;&nbsp;Release date: ' + data.tracks.items[i].album.release_date + '</p> <br>';
+        result += '<p class="card-subtitle">&nbsp;&nbsp;Avaliable: ' + parseInt(data.tracks.items[i].album.available_markets.length) + ' countries</p> <br></div></div>';
+        // console.log(result);
     }
     document.getElementById("ans").innerHTML = result;
 }
